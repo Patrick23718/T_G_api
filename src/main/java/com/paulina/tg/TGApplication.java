c43f305fc3907f3b1d5ef3c2a1fee77faa2c2d0f
Patrick23718 <noukimi.patrick@gmail.com>
2024-09-06 20:03:35 +0100

    first commit

---
+++ src/main/java/com/paulina/tg/TGApplication.java
@@ -0,0 +1,13 @@
+package com.paulina.tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulina/tg/TGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulina.tg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
